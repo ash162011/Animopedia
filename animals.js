338b7eb4874fcf53d34508f5cfaaa2be7c8e9e7d
ash162011 <83339498+ash162011@users.noreply.github.com>
2021-09-28 18:42:07 +0530

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,169 @@
+function result() {
+     var content = document.getElementById("animal_name").value;
+
+     if(content == "LION")
+     {
+          document.getElementById("lifeSpan").innerHTML = "10 - 15 years";
+          document.getElementById("scientificName").innerHTML = "Panthera leo";
+     }
+
+     else if(content == "ELEPHANT"){
+          document.getElementById("lifeSpan").innerHTML = "60 - 70 years";
+          document.getElementById("scientificName").innerHTML = "Loxodonta";
+     }
+     
+     else if(content == "TIGER"){
+          document.getElementById("lifeSpan").innerHTML = "8 - 10 years";
+          document.getElementById("scientificName").innerHTML = "Panthera tigris";
+     }
+
+     else if(content == "MONKEY"){
+          document.getElementById("lifeSpan").innerHTML = "25 - 30 years";
+          document.getElementById("scientificName").innerHTML = "Cercopithecidae";
+     }
+
+     else if(content == "KANGAROO"){
+          document.getElementById("lifeSpan").innerHTML = "10 years";
+          document.getElementById("scientificName").innerHTML = "Macropodidae";
+     }
+
+     else if(content == "HIPPOPOTAMUS"){
+          document.getElementById("lifeSpan").innerHTML = "40 - 50 years";
+          document.getElementById("scientificName").innerHTML = "Hippopotamus amphibius";
+     }
+
+     else if(content == "POLAR BEAR"){
+          document.getElementById("lifeSpan").innerHTML = "25 - 30 years";
+          document.getElementById("scientificName").innerHTML = "Ursus maritimus";
+     }
+
+     else if(content == "FOX"){
+          document.getElementById("lifeSpan").innerHTML = "14 years";
+          document.getElementById("scientificName").innerHTML = "Vulpes vulpes";
+     }
+
+     else if(content == "SNAKE"){
+          document.getElementById("lifeSpan").innerHTML = "20 - 30 years";
+          document.getElementById("scientificName").innerHTML = "Serpentes";
+     }
+
+     else if(content == "COW"){
+          document.getElementById("lifeSpan").innerHTML = "18 - 22 years";
+          document.getElementById("scientificName").innerHTML = "Bos taurus";
+     }
+
+     else if(content == "PIG"){
+          document.getElementById("lifeSpan").innerHTML = "10 - 15 years";
+          document.getElementById("scientificName").innerHTML = "Sus";
+     }
+
+     else if(content == "CAT"){
+          document.getElementById("lifeSpan").innerHTML = " 2 – 16 years";
+          document.getElementById("scientificName").innerHTML = "Felis catus";
+     }
+
+     else if(content == "DOG"){
+          document.getElementById("lifeSpan").innerHTML = " 10 – 13 years";
+          document.getElementById("scientificName").innerHTML = "Canis lupus familiaris";
+     }
+
+     else if(content == "DONKEY"){
+          document.getElementById("lifeSpan").innerHTML = " 20 – 30 years";
+          document.getElementById("scientificName").innerHTML = "Equus asinus";
+     }
+
+     else if(content == "SHEEP"){
+          document.getElementById("lifeSpan").innerHTML = " 10 – 12 years";
+          document.getElementById("scientificName").innerHTML = "Ovis aries";
+     }
+
+     else if(content == "GOAT"){
+          document.getElementById("lifeSpan").innerHTML = " 15 – 18 years";
+          document.getElementById("scientificName").innerHTML = "Capra aegagrus hircus";
+     }
+
+     else if(content == "HORSE"){
+          document.getElementById("lifeSpan").innerHTML = " 25 – 30 years";
+          document.getElementById("scientificName").innerHTML = "Equus caballus";
+     }
+
+     else if(content == "CHICKEN"){
+          document.getElementById("lifeSpan").innerHTML = " 5 – 10 years";
+          document.getElementById("scientificName").innerHTML = "Gallus gallus domesticus";
+     }
+
+     else if(content == "CAMEL"){
+          document.getElementById("lifeSpan").innerHTML = " 40 years";
+          document.getElementById("scientificName").innerHTML = "Camelus";
+     }
+
+     else if(content == "CHICKEN"){
+          document.getElementById("lifeSpan").innerHTML = " 5 – 10 years";
+          document.getElementById("scientificName").innerHTML = "Gallus gallus domesticus";
+     }
+
+     else if(content == "BUFFALO"){
+          document.getElementById("lifeSpan").innerHTML = " 5 – 10 years";
+          document.getElementById("scientificName").innerHTML = "Bubalus bubalis";
+     }
+
+     else if(content == "BEAR"){
+          document.getElementById("lifeSpan").innerHTML = " 20 – 30 years";
+          document.getElementById("scientificName").innerHTML = "Ursidae";
+     }
+
+     else if(content == "POLAR BEAR"){
+          document.getElementById("lifeSpan").innerHTML = " 25 – 30 years";
+          document.getElementById("scientificName").innerHTML = "Ursus maritimus";
+     }
+
+     else if(content == "WOLF"){
+          document.getElementById("lifeSpan").innerHTML = " 14 – 16 years";
+          document.getElementById("scientificName").innerHTML = "Canis lupus";
+     }
+
+     else if(content == "DEER"){
+          document.getElementById("lifeSpan").innerHTML = " 6 – 14 years";
+          document.getElementById("scientificName").innerHTML = "Cervidae";
+     }
+
+     else if(content == "PANDA"){
+          document.getElementById("lifeSpan").innerHTML = " 20 years";
+          document.getElementById("scientificName").innerHTML = "Ailuropoda melanoleuca";
+     }
+
+     else if(content == "SLOTH"){
+          document.getElementById("lifeSpan").innerHTML = " 20 years";
+          document.getElementById("scientificName").innerHTML = "Folivora";
+     }
+
+     else if(content == "KOALA"){
+          document.getElementById("lifeSpan").innerHTML = " 13 - 18 years";
+          document.getElementById("scientificName").innerHTML = "Phascolarctos cinereus";
+     }
+
+     else if(content == "MOOSE"){
+          document.getElementById("lifeSpan").innerHTML = "15 – 25 years";
+          document.getElementById("scientificName").innerHTML = "Alces alces";
+     }
+
+     else if(content == "RABBIT"){
+          document.getElementById("lifeSpan").innerHTML = "8 – 12 years";
+          document.getElementById("scientificName").innerHTML = "Oryctolagus cuniculus";
+     }
+
+     else if(content == "GIRAFFE"){
+          document.getElementById("lifeSpan").innerHTML = "20 – 25 years";
+          document.getElementById("scientificName").innerHTML = "Giraffa";
+     }
+
+     else if(content == "ZEBRA"){
+          document.getElementById("lifeSpan").innerHTML = "25 years";
+          document.getElementById("scientificName").innerHTML = "Equus quagga";
+     }
+
+     else if(content == "GORILLAS"){
+          document.getElementById("lifeSpan").innerHTML = "35 – 40 years";
+          document.getElementById("scientificName").innerHTML = "Equus quagga";
+     }
+}
